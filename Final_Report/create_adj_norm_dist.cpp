24d923a7b3f84bee7af9988e017600fcfcebe473
Gareth <gpmarriott3@gmail.com>
2020-04-21 18:43:19 -0700

    fix

--- Final_Report/create_adj_norm_dist.cpp
+++ Final_Report/create_adj_norm_dist.cpp
@@ -51,7 +51,7 @@ int main(int argc, char const *argv[]) {
 
 	for (int i = 0; i < size; i++) {
 		int number = 0;
-		while (number < 1) {
+		while (number < 1 && number < size-1) {
 			number = std::round(distribution(gen));
 			numOfEdges.push_back(number);
 			// printf("%d\n", number);
